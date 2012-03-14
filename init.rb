f616fe5614e988bd641c0591ef4c123fbe828772
Scott Clasen <scott@heroku.com>
2012-03-14 11:33:05 -0700

    works

--- init.rb
+++ init.rb
@@ -3,13 +3,27 @@ require 'rest_client'
 require 'net/http'
 
 class Heroku::Command::War < Heroku::Command::BaseWithApp
-
+    # push
+    #
+    # push a slug for an app
+    #
+    #  -w, --war WARFILE         # war to push
+    #
   def push
-    puts 'Push!'
-    war = extract_option('--war')
-    RestClient.post 'http://warpath.herokukapp.com/push', :appName => app,
-    :apiKey => Heroku::Auth.api_key,
-    :war => File.new(war, 'rb')
+    war = extract_option("--war")
+    if war == nil 
+        raise Heroku::Command::CommandFailed, "No .war specified.\nSpecify which war to use with --war <war file name>"
+    end
+    if !war.end_with?(".war")
+        raise Heroku::Command::CommandFailed, "War file must have a .war extension"
+    end
+      display("Pushing #{war} to #{app}")
+      response = RestClient.post 'http://warpath.herokuapp.com/push', :appName => app, :apiKey => Heroku::Auth.api_key, :war => File.new(war, 'rb')
+      if response.code == 200
+          display "Successfully pushed #{war} to #{app}"
+      else
+          display "Failed to push #{war} to #{app}"
+      end
   end
 end
 
