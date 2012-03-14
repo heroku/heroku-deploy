92d66cf39003cd811909bbaa727f350685d8a9ea
Scott Clasen <scott@heroku.com>
2012-03-14 10:55:13 -0700

    more

--- init.rb
+++ init.rb
@@ -7,7 +7,9 @@ class Heroku::Command::War < Heroku::Command::BaseWithApp
   def push
     puts 'Push!'
     war = extract_option('--war')
-    RestClient.post 'http://warpath.herokukapp.com/push', :appName => app, :apiKey => Heroku::Auth.api_key, :war => File.new(war, 'rb')
+    RestClient.post 'http://warpath.herokukapp.com/push', :appName => app,
+    :apiKey => Heroku::Auth.api_key,
+    :war => File.new(war, 'rb')
   end
 end
 
