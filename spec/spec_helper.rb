7a27aaf24edaea744ab5c0251a7aa9150c1a2f92
Joe Kutner <jpkutner@gmail.com>
2016-04-28 08:12:49 -0500

    Better test error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require "rspec"
 require "tempfile"
 require 'rspec/retry'
 
+raise "HEROKU_TEST_APP_NAME is not set!" unless ENV['HEROKU_TEST_APP_NAME']
+
 RSpec.configure do |config|
   config.verbose_retry = true # show retry status in spec process
 end
