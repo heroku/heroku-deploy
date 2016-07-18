ff72ffe4f8f9ae251de52fcc08b5ae38c24d142a
Joe Kutner <jpkutner@gmail.com>
2016-07-18 08:34:41 -0500

    Updated heroku-deploy jar to 1.0.4

--- lib/heroku/command/deploy.rb
+++ lib/heroku/command/deploy.rb
@@ -5,7 +5,7 @@ require 'net/http'
 # deploy to an app
 #
 class Heroku::Command::Deploy < Heroku::Command::BaseWithApp
-  VERSION = "0.14"
+  VERSION = "0.15"
   MAX_UPLOAD_SIZE_MB = 300
   MAX_UPLOAD_SIZE_BYTES = MAX_UPLOAD_SIZE_MB*1024*1024
   STATUS_SUCCESS = "success"
